0604d22cd89832c6051c1f78c26f7115ba0efce4
MatteoBarducci <mbardu1301@gmail.com>
2024-05-13 17:27:51 +0200

    import footer on AppVue

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script>
+    export default{
+        name: 'Footer',
+        data() {
+            return{
+            }
+        }
+    }
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables.scss' as *;
+
+
+</style>
